4cbd0e56456080360c9fc23445f59145e64ad16d
Rahul Vinod Kela <rahul.vinodkela@CT1089-233812L.local>
2025-03-12 13:38:16 +0530

    Intial Commit

---
+++ src/main/java/com/cleartrip/bootcamp_ecommerce/BootcampEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cleartrip.bootcamp_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleartrip/bootcamp_ecommerce/BootcampEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleartrip.bootcamp_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
